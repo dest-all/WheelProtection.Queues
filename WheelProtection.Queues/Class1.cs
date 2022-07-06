0a8edf424b3a058a1486d61f17e960db339722a9
DESKTOP-86AEAN3\igor_ <izhurbenko@aexsoft.com>
2022-07-06 22:23:44 +0600

    Add project files.

---
+++ WheelProtection.Queues/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WheelProtection.Queues
+{
+    public class QueueProcessor<TIn, TOut>
+    {
+        readonly Func<TIn, TOut> _do;
+        public QueueProcessor(Func<TIn, TOut> @do)
+        {
+            _do = @do;
+        }
+
+        public TOut Do(TIn argument)
+        {
+            return _do(argument);
+        }
+    }
+}
